5fc071a696af74235723b8aee384f9b459e67fd9
Pierre Feillet <FEILLET@fr.ibm.com>
2020-04-02 21:05:30 +0200

    Adding a REST method to retrieve a ML schema

--- machine-learning/ml-model-dynamic-hosting/main.py
+++ machine-learning/ml-model-dynamic-hosting/main.py
@@ -30,8 +30,8 @@ modelDictionary = dict({
     ]
 })
 
-#todo
-#Propagate the joblib metadata into the model management dictionary
+# todo
+# Propagate the joblib metadata into the model management dictionary
 
 #
 # Flask
@@ -55,26 +55,68 @@ class Model(Resource):
         """Returns the list of ML models."""
         return modelDictionary
 
-ns = api.namespace('automation/api/v1.0/prediction/generic', description='run any ML models')
+
+model_key_descriptor = api.model('ModelKeyDescriptor', {
+    'name': fields.String(required=True, description="Name of the model", help="Name cannot be blank."),
+    'version': fields.String(required=True, description="Version of the model", help="Name cannot be blank."),
+    'format': fields.String(required=True, description="Format of the model", help="Name cannot be blank.")
+})
 
 model_descriptor = api.model('ModelDescriptor', {
-    'path': fields.String(required = True, description="Local path of the model", help="Name cannot be blank."),
-    'version': fields.String(required = True, description="Version of the model", help="Name cannot be blank."),
-    'format': fields.String(required = True, description="Format of the model", help="Name cannot be blank.")
-    })
+    'path': fields.String(required=True, description="Local path of the model", help="Name cannot be blank."),
+    'version': fields.String(required=True, description="Version of the model", help="Name cannot be blank."),
+    'format': fields.String(required=True, description="Format of the model", help="Name cannot be blank.")
+})
+
+model_schema = api.model('ModelSchema', {
+    'schema': fields.String(required=True, description="Schema of the model", help="Name cannot be blank.")
+})
+
+
+@ns.route('/ModelSchema')
+class ModelSchema(Resource):
+    @api.expect(model_key_descriptor)
+    @api.response(202, 'ML Schema retrieved.', model_schema)
+    def post(self):
+        """Returns the schema of a model."""
+        json_dictionary = request.json
+        print(json_dictionary)
+
+        # Model
+        model_name = json_dictionary["name"]
+        mode_version = json_dictionary["version"]
+        model_format = json_dictionary["format"]
+
+        # Compose the model path
+        model_path = 'models/' + model_name + '.' + model_format
+
+        # Local read
+        model_dictionary = load(model_path)
+
+        # Make a copy and remove the model from it as non serializable into JSON
+        model_dictionnary_copy = model_dictionary.copy()
+        del model_dictionnary_copy["model"]
+        del model_dictionnary_copy["metadata"]["date"]
+
+        return model_dictionnary_copy
+
+
+ns = api.namespace('automation/api/v1.0/prediction/generic', description='run any ML models')
 
 prediction_request = api.model('PredictionRequest', {
     'model': fields.Nested(model_descriptor),
-    'features': fields.List(fields.Wildcard(fields.String))
-    })
+    'features': fields.Wildcard(fields.String)
+})
 
 prediction_response = api.model('PredictionResponse', {
-    'path': fields.String(required = True, description="Local path of the invoked predictive model", help="Name cannot be blank."),
-    'id': fields.String(required = True, description="Uuid of the prediction", help="Name cannot be blank."),
-    'prediction': fields.String(required = True, description="The prediction", help="Name cannot be blank."),
-    'probabilities': fields.List(fields.Wildcard(fields.String))
+    'path': fields.String(required=True, description="Local path of the invoked predictive model",
+                          help="Name cannot be blank."),
+    'id': fields.String(required=True, description="Uuid of the prediction", help="Name cannot be blank."),
+    'prediction': fields.String(required=True, description="The prediction", help="Name cannot be blank."),
+    'probabilities': fields.Wildcard(fields.String)
 
 })
+
 
 @ns.route('/')
 class PredictionService(Resource):
