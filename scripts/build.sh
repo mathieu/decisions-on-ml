65ae2d54b75e97145766e2e5f24cd83960951186
laurent grateau <laurent.grateau@fr.ibm.com>
2020-04-10 15:52:22 +0200

    Fix build without static

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ pwd
 echo "Building static apps..."
 #cd ml-service/ml-model-static-hosting
 #docker build . -t static-ml-microservice  -f ./Dockerfile --no-cache
+#cd ../../
 echo "Building dynamic apps..."
-cd ../../
 cd ml-service/ml-model-dynamic-hosting
 docker build . -t dynamic-ml-microservice  -f ./Dockerfile --no-cache
