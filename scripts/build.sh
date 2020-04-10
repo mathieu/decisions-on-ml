755369f0c14c296f69654037f82d9b0071484480
laurent grateau <laurent.grateau@fr.ibm.com>
2020-04-10 15:19:49 +0200

    Fix path

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,9 @@ ls -la
 
 echo "Building static apps..."
 cd ..
-cd decision-on-ml/ml-service/ml-model-static-hosting
+cd ml-service/ml-model-static-hosting
 docker build . -t static-ml-microservice  -f ./Dockerfile --no-cache
 echo "Building dynamic apps..."
-cd decision-on-ml/ml-service/ml-model-dynamic-hosting
+cd ../../
+cd ml-service/ml-model-dynamic-hosting
 docker build . -t dynamic-ml-microservice  -f ./Dockerfile --no-cache
