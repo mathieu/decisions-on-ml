01be07b13402cb20807bef9293a4606098513c65
laurent grateau <laurent.grateau@fr.ibm.com>
2020-04-20 16:57:30 +0200

    Deploy all in one ruleapp.

--- scripts/deployODMMaterial.sh
+++ scripts/deployODMMaterial.sh
@@ -4,7 +4,7 @@ echo "start ODM material odm-ml..."
 echo "current build directory:"
 pwd
 echo "Deploy XOM..."
-curl -k -v -H "Content-Type: application/octet-stream" --data-binary @"./decision-service-projects/Miniloan Service/output/miniloan-xom.jar" -u odmAdmin:odmAdmin http://localhost:9080/res/apiauth/v1/xoms/miniloan-xom.jar
+#curl -k -v -H "Content-Type: application/octet-stream" --data-binary @"./decision-service-projects/Miniloan Service/output/miniloan-xom.jar" -u odmAdmin:odmAdmin http://localhost:9080/res/apiauth/v1/xoms/miniloan-xom.jar
 
 echo "Deploy Ruleapp..."
-curl -k -v -H "Content-Type: application/octet-stream" --data-binary @"./decision-service-projects/Miniloan Service/output/miniloanwithml.jar" -u odmAdmin:odmAdmin http://localhost:9080/res/apiauth/v1/ruleapps
+curl -k -v -H "Content-Type: application/octet-stream" --data-binary @'./decision-service-projects/Miniloan Service/output/miniloan-with-ml-ruleapps-backup.jar' -u odmAdmin:odmAdmin http://localhost:9080/res/apiauth/v1/ruleapps
